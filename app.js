58c68129b7cdfd34cb7bb35ad2c6b8aad94fd4b1
Dhafin Rizqullah <dhafinriz050500@gmail.com>
2018-07-02 15:52:24 +0700

    commit

---
+++ app.js
@@ -0,0 +1,10 @@
+var express=require('express');
+var app=express();
+
+app.get('/helloThere',function(request,response){
+    response.send('Hello there, from express')
+});
+
+app.listen(1337,function(){
+    console.log('Listening at Port 1337');
+});
